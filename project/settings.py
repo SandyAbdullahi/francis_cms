b35b0c968e8ac49fdf3998ef92a36774d5990bd8
AbdullahinurAA <abdullahinur.abdullahi@gmail.com>
2020-09-04 10:39:01 +0300

    test deployment to heroku

--- project/settings.py
+++ project/settings.py
@@ -95,8 +95,10 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'francis',
+        'USERNAME': 'ab',
+        'PASSWORD': 'ab'
     }
 }
 
