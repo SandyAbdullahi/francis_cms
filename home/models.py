4b9c95a48195956d056cff8c19b26d00c29e0130
AbdullahinurAA <abdullahinur.abdullahi@gmail.com>
2020-09-09 21:59:27 +0300

    add image model field and display test image. test deployment

--- home/models.py
+++ home/models.py
@@ -7,9 +7,22 @@ from wagtail.admin.edit_handlers import FieldPanel
 
 class HomePage(Page):
     headline = RichTextField(blank=True)
+    headline_intro = RichTextField(blank=True)
+    headline_image = models.ForeignKey(
+        'wagtailimages.Image', null=True, blank=True,
+        on_delete=models.SET_NULL, related_name='+'
+    )
+    muted_text_top = RichTextField(blank=True)
+    top_sentence = RichTextField(blank=True)
+
+    
     body = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
         FieldPanel('headline', classname="full"),
+        FieldPanel('headline_intro', classname="full"),
+        FieldPanel('headline_image', classname="full"),
+        FieldPanel('muted_text_top', classname="full"),
+        FieldPanel('top_sentence', classname="full"),
         FieldPanel('body', classname="full"),
     ]
