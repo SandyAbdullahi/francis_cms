fb5d5920dd4e788ea116c8a9b839f424eb005a3e
AbdullahinurAA <abdullahinur.abdullahi@gmail.com>
2020-09-09 20:47:44 +0300

    add bootstrap cdn, create new field to test on homepage, test deployment

--- home/models.py
+++ home/models.py
@@ -6,8 +6,10 @@ from wagtail.admin.edit_handlers import FieldPanel
 
 
 class HomePage(Page):
+    headline = RichTextField(blank=True)
     body = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
+        FieldPanel('headline', classname="full"),
         FieldPanel('body', classname="full"),
     ]
