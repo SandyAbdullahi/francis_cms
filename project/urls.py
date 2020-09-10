fe13414977e9bd4df0c3430f900b1eac294fe788
AbdullahinurAA <abdullahinur.abdullahi@gmail.com>
2020-09-10 22:24:53 +0300

    add data transfer lib

--- project/urls.py
+++ project/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, re_path, include
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
-
+from wagtail_transfer import urls as wagtailtransfer_urls
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -28,6 +28,7 @@ urlpatterns = [
     path('admin/', include(wagtailadmin_urls)),
     path('documents/', include(wagtaildocs_urls)),
     re_path(r'', include(wagtail_urls)),
+    path('wagtail-transfer/', include(wagtailtransfer_urls)),
     # path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
