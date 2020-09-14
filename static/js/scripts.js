d0470888f6605643df0abd68906848d9a4e3f522
AbdullahinurAA <abdullahinur.abdullahi@gmail.com>
2020-09-14 19:45:48 +0300

    add date time field, install bootstrap tempus

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,9 +1,7 @@
 mdc.ripple.MDCRipple.attachTo(document.querySelector('.foo-button'));
-
-
-// import the require modules for material design
-const formField = mdc.formField.MDCFormField;
-const checkbox = mdc.checkbox.MDCCheckbox;
-
-
-console.log(checkbox);
+$(function () {
+    $('#datetimepicker13').datetimepicker({
+        inline: true,
+        sideBySide: true
+    });
+});
