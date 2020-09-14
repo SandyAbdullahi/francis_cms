d3d8bbf7f6da32ce809070609b9d28b9429999e7
AbdullahinurAA <abdullahinur.abdullahi@gmail.com>
2020-09-14 19:05:14 +0300

    add material design test contact form and submit for deployment

---
+++ static/js/scripts.js
@@ -0,0 +1,9 @@
+mdc.ripple.MDCRipple.attachTo(document.querySelector('.foo-button'));
+
+
+// import the require modules for material design
+const formField = mdc.formField.MDCFormField;
+const checkbox = mdc.checkbox.MDCCheckbox;
+
+
+console.log(checkbox);
