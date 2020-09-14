15d855450a850c884654441bb10a7b65ada4f383
AbdullahinurAA <abdullahinur.abdullahi@gmail.com>
2020-09-14 21:35:45 +0300

    remove padding from submit btn

--- contact/models.py
+++ contact/models.py
@@ -1,13 +1,44 @@
 from django.db import models
 
-from wagtail.core.models import Page
-from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
+from modelcluster.fields import ParentalKey
+
+from wagtail.core.models import Page, Orderable
+from wagtail.core.fields import RichTextField, StreamField
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 
 class ContactPage(Page):
-    body = RichTextField(blank=True)
+   
+    headline_title = RichTextField(blank=True)
+    headline_message = RichTextField(blank=True)
+
+
+
+
 
     content_panels = Page.content_panels + [
-        FieldPanel('body', classname="full"),
+        MultiFieldPanel([
+            FieldPanel('headline_title', classname="full"),
+            FieldPanel('headline_message', classname="full"),
+        ], heading="Headline Content")
+    ]
+
+class ServiceItem(Page):
+    service_image = models.ForeignKey(
+        'wagtailimages.Image', null=True, blank=True,
+        on_delete=models.SET_NULL, related_name='+'
+    )
+    service_email_name = RichTextField(blank=True)
+    service_title = RichTextField(blank=True)
+    service_description = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+       MultiFieldPanel([
+        ImageChooserPanel("service_image", classname="full"),
+        FieldPanel('service_email_name', classname="full"),
+        FieldPanel('service_title', classname="full"),
+        FieldPanel('service_description', classname="full"),
+       ], heading="Service choices")
+
     ]
